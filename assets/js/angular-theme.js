c0e19ceecd9deaf0fa5a3a7e1a97364de8d15e35
ommoh <omoh128@gmail.com>
2018-04-23 08:11:42 +0200

    my commit

---
+++ assets/js/angular-theme.js
@@ -0,0 +1,27 @@
+var wpApp = new angular.module( 'wpAngularTheme', ['ui.router', 'ngResource'] );
+
+wpApp.factory( 'Posts', function( $resource ) {
+	return $resource( appInfo.api_url + 'posts/:ID', {
+		ID: '@id'
+	})
+});
+
+wpApp.controller( 'ListCtrl', ['$scope', 'Posts', function( $scope, Posts ) {
+	console.log('ListCtrl');
+	$scope.page_title = 'Blog Listing Page';
+
+	Posts.query(function( res ) {
+		$scope.posts = res;
+	});
+	
+}]);
+
+wpApp.config( function( $stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/');
+	$stateProvider
+		.state( 'list', {
+			url: '/',
+			controller: 'ListCtrl',
+			templateUrl: appInfo.template_directory + 'templates/list.html'
+		})
+})
